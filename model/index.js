e4e4d5b6a8253b125a20cef88930baf4c2719e6f
abdulrehman03365 <31584725+abdulrehman03365@users.noreply.github.com>
2023-01-19 01:16:59 +0500

    Structure the coed

--- model/index.js
+++ model/index.js
@@ -1,4 +1,8 @@
+const { default: mongoose } = require('mongoose')
+
+mongoose=require('mongoose')
 const db ={}
+db.mongoose=mongoose
 db.Roles =['User','Admin','Owner']
 db.user=require('./user.model')
 db.role=require('./role.model')
