762100692fe46ab03eefe9451b2fd79c74157bb1
abdulrehman03365 <[abdulrehman03365@gmail.com]>
2023-02-10 14:51:47 +0500

    add react-form-hook

--- client/src/pages/singUp/signUp.js
+++ client/src/pages/singUp/signUp.js
@@ -1,40 +1,49 @@
 import { Component } from "react";
 import './signUp.css'
-class SignUp extends Component{
+import {useform} from "react-hook-form"
 
-    render(){
-        return(
- <div class="signUpForm">
-      <form action="api/auth/signup" method="post" class="form">
+function SignUpform(){
+
+const {register, handleSubmit , formState : {errors}}=useform();
+
+const onSubmit = (data)=>console.log(data);
+
+
+  return(
+    <>
+     <div class="signUpForm" >
+      <form class="form" onSubmit={handleSubmit(onSubmit)}>
         <h1 class="title">Sign up</h1>
   
         <div class="inputContainer">
-          <input type="text" class="input" name ="email" required placeholder="a"/>
+          <input type="text" {...register("email",{required:true})}    class="input" name ="email" required />
           <label for="" class="label">Email</label>
         </div>
   
         <div class="inputContainer">
-          <input type="text" class="input" name="username" required placeholder="a"/>
+          <input type="text" {...register("username",{required:true})}  class="input" name="username" required />
           <label for="" class="label">Username</label>
         </div>
   
         <div class="inputContainer">
-          <input type="text" class="input" name="password" required minlength="8" placeholder="a"/>
+          <input type="text" {...register("password",{required:true , minlength:8})}  class="input" name="password" required minlength="8"/>
           <label for="" class="label">Password</label>
         </div>
   
         <div class="inputContainer">
-          <input type="text" class="input" placeholder="a"/>
+          <input type="text" {...register("password",{required:true , minlength:8})} class="input"/>
           <label for="" class="label">Confirm Password</label>
         </div>
   
         <input type="submit" class="submitBtn" value="Sign up"/>
       </form>
     </div> 
-
-        )
-
-}
+    </>
+  )
 }
 
-export default SignUp;
+
+       
+
+
+export default SignUpform;
