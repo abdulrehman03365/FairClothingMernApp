9f77a91659d1b80c54ab08ca1c2d68c6581ea31b
abdulrehman03365 <[abdulrehman03365@gmail.com]>
2023-02-16 20:05:54 +0500

    Learn useEffect

--- client/src/components/becomePartner/becomePartner.js
+++ client/src/components/becomePartner/becomePartner.js
@@ -1,8 +1,37 @@
 import { Component } from "react";
+import { useState, useEffect } from "react";
 
-function BecomePartner()
+function BecomePartner({value})
 {
-    return<h1>Welcome to become Partner page</h1>
+  const [selection,setSelection]=useState("")
+  
+   useEffect(
+   async () => {
+
+    try {
+        const response = await fetch('https://api.npms.io/v2/search?q=react');
+      console.log(response);
+    } catch (error) {
+        // TypeError: Failed to fetch
+        console.log('There was an error', error)
+      } 
+    
+    },[])
+ 
+
+
+    return(
+   
+    <div>
+    <h1>Welcome to become Partner page</h1>
+    <input type="radio" name="option" value={"a"}  onChange={(event)=>{setSelection(event.target.value)}} />Male
+
+    <p></p>
+    </div>
+    
+    )
+  
+   
 }
 
 export default BecomePartner
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,22 +1,25 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-class Home extends Component{
+import { useState } from "react";
+import BecomePartner from "../becomePartner/becomePartner";
+function Home(){
 
-    render(){
+ const [value,setValue]=useState("initial value")
         return( 
             <div>
- <h1>Welcome to Booking website Home Page</h1>
+        <h1>Welcome to Booking website Home Page</h1>
         <ul className="navbar-nav mr-auto">
           <li><Link to={'/'} className="nav-link"> Home </Link></li>
           <li><Link to={'/signUp'} className="nav-link">SignUp</Link></li>
           <li><Link to={'/signIn'} className="nav-link">SignIn</Link></li>
           <li><Link to={'/becomePartner'} className="nav-link">BecomePartner</Link></li>
         </ul>
+
             </div>
        )
     }
     
     
-}
+
 
 export default Home
