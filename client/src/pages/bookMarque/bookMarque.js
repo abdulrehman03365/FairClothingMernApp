551481aa11a38db14fbe45b4fd8943193b5eef42
abdulrehman03365 <[abdulrehman03365@gmail.com]>
2023-03-03 18:52:27 +0500

    flex view added

--- client/src/pages/bookMarque/bookMarque.js
+++ client/src/pages/bookMarque/bookMarque.js
@@ -1,62 +1,43 @@
-import { Component } from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './bookMarque.css'
 import Nevbar from "../../components/navBar/nevbar";
 import MarqueView from "../../components/marqueView/marqueView";
-function bookMarque()
-{
- 
-  // const [viewCount,setCount]=useState([])
- const marqueDetails=[{
-  name:"Niaz Marque",
-  location :'Adiala Road',
-  img:"./marqueImages/bilal.jpg"} ,
-{name:"Niaz Marque",
-location :'Adiala Road',
-img:"client/public/marqueImages/bilal.jpeg"},
-{name:"Niaz Marque",
-location :'Adiala Road',
-img:"client/public/marqueImages/bilal.jpeg"},
-{name:"Niaz Marque",
-location :'Adiala Road',
-img:"client/public/marqueImages/bilal.jpeg"}]
 
-//  async function loadMarqueData()
-//    {
+function BookMarque() {
+  const marqueDetails = [
+    {
+      name: "Niaz Marque",
+      location: "Adiala Road",
+      img: "./marqueImages/bilal.jpg"
+      
+    },
+    {
+      name: "Niaz Marque",
+      location: "Adiala Road",
+      img: "./marqueImages/bilal.jpg"
+    },
+    {
+      name: "Niaz Marque",
+      location: "Adiala Road",
+      img: "./marqueImages/bilal.jpg"
+    },
+    {
+      name: "Niaz Marque",
+      location: "Adiala Road",
+      img: "./marqueImages/bilal.jpg"
+    }
+  ];
 
-//   //  response= await fetch()
-    
-  
-
-//    }
-   
-//  useEffect(
-
-// ()=>{
-
-// loadMarqueData()
-
-// }
-
-//  )
-
-
-
-    return(
-   
+  return (
     <div>
-    <Nevbar></Nevbar>
-  
-    <div id="MarquePlaceHolder">
-    <MarqueView marqueDetail={marqueDetails[0]} />
-    {/* {marqueDetails.map((marqueDetail)=>{<MarqueView marqueDetail={marqueDetail} />})} */}
+      <Nevbar />
+      <div id="MarquePlaceHolder">
+        {marqueDetails.map((marqueDetail) => (
+          <MarqueView marqueDetail={marqueDetail} />
+        ))}
+      </div>
     </div>
-
-    </div>
-    
-    )
-  
-   
+  );
 }
 
-export default bookMarque
+export default BookMarque;
