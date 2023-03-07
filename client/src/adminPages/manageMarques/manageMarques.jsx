800fe5387154e8c0d967d442fdfe932a6c9a2617
abdulrehman03365 <31584725+abdulrehman03365@users.noreply.github.com>
2023-03-07 07:55:12 +0500

    Form validation

--- client/src/adminPages/manageMarques/manageMarques.jsx
+++ client/src/adminPages/manageMarques/manageMarques.jsx
@@ -2,15 +2,18 @@ import AdminHeader from "../../components/adminComponents/adminHeader/adminHeade
 import Preview from "../../components/adminComponents/preview/preview";
 import {useForm} from "react-hook-form"
 import './manageMarques.css'
+import { useState } from "react";
 function ManageMarques() {
 
+  const [imagePreview,setImagePreview]=useState(null)
+  const []=useState(null)
 
-  const handleSubmit=()=>
-   {
-
-   }
-
-
+  function handleSubmit(data,event) {
+    event.preventDefault()
+  } 
+  const handleImageChange=()=>{
+    
+  }
     return ( 
      <div>
         <AdminHeader/>
@@ -24,22 +27,28 @@ function ManageMarques() {
             <div className="add-marque-form" style={{"marginTop":50 , "display":"flex" ,
        "flexDirection":"column" , "alignContent":"center"  , "justifyContent":"center" , "marginRight":"30%" , "marginLeft":"30%"}}>
 
-            <input className="add-marque-input"></input>
+            <input className="add-marque-input" {...register("name",{required:true})}></input>
             <label>Marque Name</label>
+            {errors.name?.type==='required' && <p role={'alert'}>name is required</p>}
            
-            <input className="add-marque-input"></input>
+            <input className="add-marque-input" {...register("location",{required:true})}></input>
             <label>Marque Location</label>
+            {errors.location?.type==='required' && <p role={'alert'}>Location is required</p>}
             
-            <input className="add-marque-input"></input>
+            <input className="add-marque-input" {...register("capacity",{required:true})}></input>
             <label>Capacity</label>
+            {errors.capacity?.type==='required' && <p role={'alert'}>Capacity is required</p>}
 
-            <input className="add-marque-input"></input>
+            <input className="add-marque-input" {...register("status",{required:true})}></input>
             <label>Status</label>
+            {errors.status?.type==='required' && <p role={'alert'}>Status is required</p>}
 
             <label htmlFor="image">Image: </label>
-            <input type="file" id="imageInput" ></input>
-
+            <input type="file" id="imageInput" onChange={handleImageChange} {...register("image",{required:true})}></input>
+            {imagePreview && (
+            <img src={imagePreview} alt="Image preview" style={{ width: '200px' }} /> )}
             <input type={"submit"} value="Add Marque"></input>
+
 
 
            
