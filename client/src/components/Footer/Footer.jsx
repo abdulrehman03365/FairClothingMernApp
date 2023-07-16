f433dacf343bd5d0999ad02cacbb6511c040726b
abdulrehman03365 <[abdulrehman03365@gmail.com]>
2023-07-16 14:47:07 +0500

    Modify footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee , faEnvelope,faMobilePhone } from '@fortawesome/free-solid-svg-icons'
 import { DropdownButton , Dropdown , Button} from "react-bootstrap"
 import './footer.css'
 export default function Footer()
@@ -6,40 +8,41 @@ export default function Footer()
 
     return(
         
-        <>
-        <div className="Footer-Container"  style={{display:"flex",marginTop:"auto" , flexDirection:"column"  ,alignItems:"center"}}>
-       
-         <footer>
-          <div className="Footer-top" style={{"display":"flex"  , gap:"10vw", flexDirection:"row"}} >
-          <div className="Contact-div">
-          <h5 className="Footer-Headings">Contact Us </h5>
-            <ul className="Footer-List">
-              <li className="Footer-ListItems">welisten@FairClothing.com</li>
-              <li className="Footer-ListItems">+92 3022448152</li>
-            </ul>
-          </div>
-          <div className="Info-div">
-            <h5 className="Footer-Heading">Information</h5>
-            <ul className="Footer-List">
-              <li className="Footer-ListItems">About Us</li>
-              <li className="Footer-ListItems">Privacy Policy</li>
-            </ul>
-          </div>
-          <div className="Newsletter-div">
-            <h5 className="Footer-Heading">NewsLetter SignUp</h5>
-            <ul className="Footer-List">Subscribe to our  Newsletter for Exclusive Updates</ul>
-          </div>
-          </div>
-         
-         <div class="Footer-bottom"  style={{"textAlign":"center" }}>
-         <p>© All Rights Reserved by Developer <a href="#">Abdul-Rehman.</a></p>
-
-</div>
-         </footer>
-
+       <>
+       <footer className="Footer-Container">
+      <div className="Footer-top">
+        <div className="Contact-div">
+          <h5 className="Footer-Heading">Contact Us</h5>
+          <ul className="Footer-List">
+            <li className="Footer-ListItem">
+              <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '0.3vw' }} />
+              welisten@FairClothing.com
+            </li>
+            <li className="Footer-ListItem">
+              <FontAwesomeIcon icon={faMobilePhone} style={{ marginRight: '0.3vw' }} />
+              +92 3022448152
+            </li>
+          </ul>
         </div>
-       
-        </>)
+        <div className="Info-div">
+          <h5 className="Footer-Heading">Information</h5>
+          <ul className="Footer-List">
+            <li className="Footer-ListItem">About Us</li>
+            <li className="Footer-ListItem">Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="Newsletter-div">
+          <h5 className="Footer-Heading">Newsletter SignUp</h5>
+          <p className="Footer-Description">Subscribe to our Newsletter for Exclusive Updates</p>
+        </div>
+      </div>
+      <div className="Footer-bottom">
+        <p>
+          © All Rights Reserved by Developer <a href="#">Abdul-Rehman.</a>
+        </p>
+      </div>
+    </footer>
+       </>)
 }
 
 
