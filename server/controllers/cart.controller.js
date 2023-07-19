e9880623db71af25af2b36c28ed1c19ca61ca03b
abdulrehman03365 <[abdulrehman03365@gmail.com]>
2023-07-19 15:36:04 +0500

    local changes

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,5 +1,14 @@
-createCart=()=>{
+import cart from '../model/cart.model'
 
+async function craeteCart(req,res,next){
+try{
+const newCart = new cart()
+await newCart.save()
+res.status(200).json({message:'Cart created successfully', cart:newCart})
+}
+catch(error){
+console.error("Error creating cart", error);
+}
 }
 
 getCart=()=>{
